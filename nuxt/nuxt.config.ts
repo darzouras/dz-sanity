24aedbc01142fad96da983157c41d1a6f2763d67
Darlene Zouras <dzouras@gmail.com>
2022-06-08 14:16:27 -0500

    update: successfully querying

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -2,5 +2,10 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  modules: ['@nuxtjs/sanity'],
+  sanity: {
+    projectId: 'zugazhip',
+    apiVersion: '2022-03-25',
+    dataset: 'production'
+  }
 })
---
+++ nuxt/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Hi</h1>
+    <div v-if="data">
+      <pre>
+        {{ JSON.stringify(data, null, 2)}}
+      </pre>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    const query = groq`*[_type == "page" && title == "Home"]`
+    const { data } = useSanityQuery(query)
+
+    return { data }
+  },
+}
+</script>
